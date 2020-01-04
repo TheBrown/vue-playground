de2f7782dee5ce252edabdf3873bf731ca55ed70
Saleumsack <saleumsack@bcel.com.la>
2020-01-04 12:17:35 +0700

    v-on:click, @click

--- src/views/EventMethod.vue
+++ src/views/EventMethod.vue
@@ -2,15 +2,31 @@
 	<div app="demo2">
 		<h1>Event and Method</h1>
 		<button v-on:click="clickMe">ClickMe</button>
+		<button v-on:click="clickMe2('Welcome to Vuejs')">ClickMe2</button>
+		<button @click="clickMe2('Welcome Bobby')">ClickMe3</button>
+		<h3>{{getTimestamp()}}</h3>
 	</div>
 </template>
 
 <script>
     export default {
         name: "EventMethod",
-		methods: {
+		mounted() {
+            this.initial()
+        },
+        methods: {
+            initial() {
+				// eslint-disable-next-line no-console
+              console.log("Initialization...")
+			},
             clickMe() {
                 alert("hi")
+			},
+			clickMe2(message) {
+                alert(message)
+			},
+			getTimestamp() {
+                return Date()
 			}
 		}
     }
