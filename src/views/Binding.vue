29f9e64fbbb55a021758bb2edabfe08d85b74ef2
Saleumsack <saleumsack@bcel.com.la>
2020-01-05 15:32:24 +0700

    Binding CSS class and style

--- src/views/Binding.vue
+++ src/views/Binding.vue
@@ -3,6 +3,9 @@
 		<h2>Binding</h2>
 		<img v-bind:src="imgUrl">
 		<a href="https://saleumsack.com">My Website</a>
+		<div :class="[errorClass, important]">Saleumsack</div>
+		<div :style="isActive ? newStyle : '' ">Watch me!</div>
+		<button @click="isActive = !isActive">Active/InActive</button>
 	</div>
 </template>
 
@@ -12,6 +15,13 @@
 		data() {
             return {
                 imgUrl: "https://www.bcel.com.la/bcel/bcelweb-datas/slide-show/imgs/bcel_web_file_62_32cf52101497471084a86ce9e1e4753d.jpg",
+				errorClass: "error",
+				important: "important",
+				newStyle: {
+                    backgroundColor: 'greenyellow',
+					color: 'White'
+				},
+				isActive: false
 			}
 		}
     }
@@ -20,5 +30,13 @@
 <style scoped>
 	img	{
 		width: 100%;
+	}
+	.error {
+		background-color: cornflowerblue;
+		color: white;
+	}
+
+	.important {
+		font-weight: bold;
 	}
 </style>
